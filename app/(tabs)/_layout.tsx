d7a4b3b4b6396691dd2e3b1ca57186de17c63046
nmkhwanazi023 <nmkhwanazi023@student.wethinkcode.co.za>
2024-07-26 22:52:49 +0200

    added icons on the tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 const Layout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="chats"/>
-      <Tabs.Screen name="support"/>
-      <Tabs.Screen name="orders"/>
-      <Tabs.Screen name="products"/>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Chats",
+          headerShown: false,
+          tabBarIcon: () => (
+            <Ionicons name="chatbox-ellipses-outline" color="black" size={25} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="support"
+        options={{
+          tabBarIcon: () => (
+            <Ionicons name="chatbox-ellipses-outline" color="black" size={25} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="orders"
+        options={{
+          tabBarIcon: () => (
+            <Ionicons name="chatbox-ellipses-outline" color="black" size={25} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="products"
+        options={{
+          tabBarIcon: () => (
+            <Ionicons name="chatbox-ellipses-outline" color="black" size={25} />
+          ),
+        }}
+      />
     </Tabs>
   );
-}
+};
 
-export default Layout
+export default Layout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout() {
         }}
       />
       <Stack.Screen name="[name]" />
-      <Stack.Screen name="(Tabs)" options={{headerShown:false}} />
+      <Stack.Screen name="(tabs)" options={{headerShown:false}} />
     </Stack>
   );
 }
